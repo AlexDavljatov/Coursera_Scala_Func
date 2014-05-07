9287fc583ae3d3619a1842cf248657a00b7f2ee7
Alexander <alexander.davljatov@gmail.com>
2014-05-07 12:46:05 +0400

    Third function is changed

--- 1/recfun/src/main/scala/recfun/Main.scala
+++ 1/recfun/src/main/scala/recfun/Main.scala
@@ -36,9 +36,8 @@ object Main {
    * Exercise 3
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-    if (money < 0) 0
-    else if (money == 0) 1
-    else if (coins.isEmpty) 0
+    if (money == 0) 1
+    else if (money < 0 || coins.isEmpty) 0
     else countChange(money - coins.head, coins) + countChange(money, coins.tail)
   }
 }
